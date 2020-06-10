3e8bf66434627a072d5d57b295641eb817d7e8a3
Alan Konhevalic <alan.patrick.konhevalic@gmail.com>
2020-06-10 18:42:38 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,105 @@
+const express = require("express")
+const server = express()
+
+//pegar o banco de dados
+const db = require("./database/db")
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//habilitando o uso do req.body da aplicação
+server.use(express.urlencoded({ extended: true }))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminho da aplicação
+//página inicial:
+//req: requisição
+//res: resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Seu Marketplace de Coleta de Resíduos" })
+})
+
+server.get("/create-point", (req, res) => {
+    //req.query: query strings da url
+    console.log(req.query)
+
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+    console.log(req.body)
+    
+    //inserindo dados no banco de dados
+     // //inserir dados na tabela
+    const query = `
+        INSERT INTO places(
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES(
+            ?, ?, ?, ?, ?, ?, ?
+        );
+    `
+
+    const values = [
+       req.body.image,
+       req.body.name,
+       req.body.address,
+       req.body.address2,
+       req.body.state,
+       req.body.city,
+       req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+    db.run(query, values, afterInsertData)
+
+    return res.render("create-point.html", {saved: true})
+})
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+
+    if(search ==" ") {
+        //pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+    //pegar os dados do banco de dados
+        db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+        //mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+    
+})
+
+
+//ligar o servidor
+server.listen(3000)
